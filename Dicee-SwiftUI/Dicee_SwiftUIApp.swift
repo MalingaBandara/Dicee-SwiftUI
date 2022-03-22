b8204814f53b97a9ed3c8203d84b8b240f62b8f5
Malinga Bandara <65806631+MalingaBandara@users.noreply.github.com>
2022-03-22 06:12:24 +0530

    init

---
+++ Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Malinga Bandara on 2022-03-22.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
